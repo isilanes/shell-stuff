06b3de712dcb32c0119b8c992afa23ca7680c72a
Iñaki Silanes <isilanes@gmail.com>
2017-03-21 17:39:02 +0100

    pyenv.sh: manage incorrect arguments properly

--- zsh/pyenv.sh
+++ zsh/pyenv.sh
@@ -1,2 +1,11 @@
-source ~/local/python/virtualenv/$1/bin/activate
-PS1=$'%{\e[35m%}[$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
+VENV_DIR=$HOME/local/python/virtualenv
+ACTIVATE=$VENV_DIR/$1/bin/activate
+
+if [[ -f $ACTIVATE ]]; then
+    source $ACTIVATE
+    PS1=$'%{\e[35m%}[$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
+else
+    echo No such virtualenv
+    echo Available venvs:
+    ls $VENV_DIR
+fi
