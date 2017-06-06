86dedf4d3a4b5ca2be69a3665b42d025c8169266
Iñaki Silanes <isilanes@gmail.com>
2017-06-06 18:53:00 +0200

    pyenv.sh: adapt to both zsh and bash

--- zsh/pyenv.sh
+++ zsh/pyenv.sh
@@ -3,7 +3,11 @@ ACTIVATE=$VENV_DIR/$1/bin/activate
 
 if [[ -f $ACTIVATE ]]; then
     source $ACTIVATE
-    PS1=$'%{\e[35m%}[$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
+    if [[ "x$SHELL" == "x/usr/bin/zsh" ]]; then
+        PS1=$'%{\e[35m%}[$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
+    elif [[ "x$SHELL" == "x/bin/bash" ]]; then
+        PS1=$'\[\e[35m\][$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
+    fi
 else
     echo No such virtualenv
     echo Available venvs:
