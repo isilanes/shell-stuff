0a6147fa2e2537f0193355480d2bd0874af2f4f1
isilanes <isilanes@gmail.com>
2017-12-02 15:54:49 +0100

    minor changes for tmux/pyenv/powerline

--- zsh/pyenv.sh
+++ zsh/pyenv.sh
@@ -5,11 +5,11 @@ ACTIVATE=$VENV_DIR/$1/bin/activate
 
 if [[ -f $ACTIVATE ]]; then
     source $ACTIVATE
-    if [[ "x$SHELL" == "x/usr/bin/zsh" ]]; then
-        PS1=$'%{\e[35m%}[$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
-    elif [[ "x$SHELL" == "x/bin/bash" ]]; then
-        PS1=$'\[\e[35m\][$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
-    fi
+    #if [[ "x$SHELL" == "x/usr/bin/zsh" ]]; then
+    #    PS1=$'%{\e[35m%}[$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
+    #elif [[ "x$SHELL" == "x/bin/bash" ]]; then
+    #    PS1=$'\[\e[35m\][$(basename \"$VIRTUAL_ENV\")] '$_OLD_VIRTUAL_PS1
+    #fi
 else
     echo No such virtualenv
     echo Available venvs:
